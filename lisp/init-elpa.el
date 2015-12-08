d660344bd0f7108e76c81da42b314d7bd8ff425c
jaypume <i@jaypu.com>
2015-12-08 18:15:29 +0800

    取消melpa的稳定源

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -24,7 +24,7 @@
 
 ;;; Also use Melpa for most packages
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+;; (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
 
 
 
