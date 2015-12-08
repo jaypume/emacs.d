92e38e548bc9ad06320060020ccf4b3e82e4b25d
jaypume <i@jaypu.com>
2015-12-08 19:57:49 +0800

    修改默认的字体大小为14

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,5 +1,7 @@
 (setq window-system-default-frame-alist
-      '((x (font . "文泉驿等宽微米黑 11")) ;; if frame created on x display
+      '((x (font . "文泉驿等宽微米黑 14")) ;; if frame created on x display
         (nil))) ;; if on term
 
+;; 修改默认的字体大小为14
+(set-default-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
 (provide 'init-fonts)
