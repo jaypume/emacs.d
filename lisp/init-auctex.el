9c498413ac3fe7a22704e2fe9acfa79be196dfae
jaypume <i@jaypu.com>
2015-12-08 16:11:22 +0800

    增加了auctex 和 multi-term支持

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -2,6 +2,41 @@
 ;; (load "preview-latex.el" nil t t)
 (require-package 'auctex)
 ;; (require 'auctex-autoloads)
+
+;;2015.12.04 设置latex的环境变量
+(setenv "PATH" (concat "/Library/TeX/texbin:" (getenv "PATH")))
+(setq exec-path (append '("/Library/TeX/texbin:") exec-path))
+
+;;2015.12.06 添加latexmk编译方式
+;; make latexmk available via C-c C-c
+;; Note: SyncTeX is setup via ~/.latexmkrc (see below)
+(require 'auctex-latexmk)
+(auctex-latexmk-setup)
+(add-hook 'LaTeX-mode-hook (lambda ()
+  (push
+    '("latexmk" "latexmk -pdf %s" TeX-run-TeX nil t
+      :help "Run latexmk on file")
+    TeX-command-list)))
+(add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk")))
+
+;; 2015.12.07 添加xelatex编译
+(add-hook 'LaTeX-mode-hook 
+          (lambda()
+             (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+             (setq TeX-command-default "XeLaTeX")
+             (setq TeX-save-query nil)
+             (setq TeX-show-compilation t)))
+
+;; 2015.12.07 使用skim为默认的查看方式，并启动emacs的服务器模式，使得skim可以和emacs交互
+;; Skim's displayline is used for forward search (from .tex to .pdf)
+;; option -b highlights the current line; option -g opens Skim in the background  
+(setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
+(setq TeX-view-program-list
+     '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+(server-start); start emacs in server mode so that skim can talk to it
+
+
+;;Latex模式设置  
 (mapc (lambda (mode)
       (add-hook 'LaTeX-mode-hook mode))
       (list ;; 'auto-fill-mode
