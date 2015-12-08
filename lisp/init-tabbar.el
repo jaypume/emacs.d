59bea5d8f19844a57742703ca19c2663c534ec8d
jaypume <i@jaypu.com>
2015-12-08 19:32:37 +0800

    修改tabbar的快捷键设置

--- lisp/init-tabbar.el
+++ lisp/init-tabbar.el
@@ -1,9 +1,20 @@
 (require-package 'tabbar)
 ;; (require 'tabbar)
 (tabbar-mode 't)
-(global-set-key (kbd "C-c n") 'tabbar-forward)
-(global-set-key (kbd "C-c p") 'tabbar-backward)
-(global-set-key (kbd "C-c f") 'tabbar-forward-tab)
-(global-set-key (kbd "C-c b") 'tabbar-backward-tab)
+(global-set-key (kbd "<M-up>") 'tabbar-backward-group)
+(global-set-key (kbd "<M-down>") 'tabbar-forward-group)
+(global-set-key (kbd "<M-left>") 'tabbar-backward)
+(global-set-key (kbd "<M-right>") 'tabbar-forward)
+
+;; 把自己打开的文件放入一个group
+(setq tabbar-buffer-groups-function
+    (lambda () (list "All Buffers")))
+
+;; (setq tabbar-buffer-list-function
+;;     (lambda ()
+;;         (remove-if
+;;           (lambda(buffer)
+;;              (find (aref (buffer-name buffer) 0) " *"))
+;;           (buffer-list))))
 
 (provide 'init-tabbar)
