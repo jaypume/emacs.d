b9e41b9d705265a6c4f53affddb0d687eba8c661
jaypume <i@jaypu.com>
2015-12-08 19:30:06 +0800

    修复了magit识别git的路径问题，增加了注释和反注释的快捷键

--- init.el
+++ init.el
@@ -15,6 +15,43 @@
 ;;2015.12.08 解决中文终端乱码的问题
 (setenv "LANG" "zh_CN.UTF-8")
 
+;;2015.12.08 设置环境变量为git
+;;; Set localized PATH for OS X
+(defun my-add-path (path-element)
+  "Add the specified PATH-ELEMENT to the Emacs PATH."
+  (interactive "DEnter directory to be added to path: ")
+  (if (file-directory-p path-element)
+     (progn
+       (setenv "PATH" (concat (expand-file-name path-element) path-separator (getenv "PATH")))
+       (add-to-list 'exec-path (expand-file-name path-element)))))
+
+(if (fboundp 'my-add-path)
+   (let ((my-paths (list "/opt/local/bin" "/usr/local/bin" "/usr/local/git/bin")))
+      (dolist (path-to-add my-paths (getenv "PATH"))
+        (my-add-path path-to-add))))
+
+;;2015.12.08 设置注释或者反注释快捷键
+(global-set-key (kbd "C-c C-/") 'comment-or-uncomment-region)
+(defun my-comment-or-uncomment-region (beg end &optional arg)
+  (interactive (if (use-region-p)
+                   (list (region-beginning) (region-end) nil)
+                 (list (line-beginning-position)
+                       (line-beginning-position 2))))
+  (comment-or-uncomment-region beg end arg)
+)
+(global-set-key [remap comment-or-uncomment-region] 'my-comment-or-uncomment-region)
+
+
+
+;;----------------------------------------------------------------------------
+;; 普杰的配置  the end
+;;----------------------------------------------------------------------------
+
+
+
+
+
+
 ;;----------------------------------------------------------------------------
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
