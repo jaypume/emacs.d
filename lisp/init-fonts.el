34996710b4622f1648ddb25a884d22328b3f8c7a
普杰 <PUJIE@HEIZI-MBP.local>
2015-12-14 04:29:46 +0800

    修改mac下的字体设置，强迫症而已

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,7 +1,8 @@
 ;; mac 下的设置
 (if (eq system-type 'darwin)
     ;; 修改默认的字体大小为14
-    (set-default-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+    ;; (set-default-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+    (set-default-font "Menlo-14")
     (set-fontset-font "fontset-default" 'han '("STHeiti"))
     )
 
