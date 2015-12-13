2774e91b0b40ab2b0b492c1ba08ca34b5672ddfa
普杰 <PUJIE@HEIZI-MBP.local>
2015-12-14 03:34:29 +0800

    根据不同的系统适配不同的字体

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,8 +1,17 @@
-(setq window-system-default-frame-alist
-      '((x (font . "文泉驿等宽微米黑 14")) ;; if frame created on x display
-        (nil))) ;; if on term
+;; mac 下的设置
+(if (eq system-type 'darwin)
+    ;; 修改默认的字体大小为14
+    (set-default-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+    (set-fontset-font "fontset-default" 'han '("STHeiti"))
+    )
 
-;; 修改默认的字体大小为14
-(set-default-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
-(set-fontset-font "fontset-default" 'han '("STHeiti"))
+;; linux 下的设置
+(if (eq system-type 'gun/linux)
+    )
+
+;; windows 下的设置
+(if (eq system-type 'windows-nt)
+    )
+
+;; 提供插件
 (provide 'init-fonts)
