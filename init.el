64b2fb1937191a1bb57b03200ae1c3493ac0b6fd
PUJIE <i@jaypu.com>
2016-06-02 09:07:39 +0800

    注释掉了绝对路径，不知道之前为什么要添加这个绝对路径

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 ;;------------------------------------------------------------------------------
 ;;pujie: 设定用户目录,加载所有普杰的设置
 ;;------------------------------------------------------------------------------
-(if(eq system-type 'darwin)
-    (setq user-emacs-directory "/Users/PUJIE/Dropbox/Libraries/dotFiles/emacs.pujie.d/")
-  )
+;;(if(eq system-type 'darwin)
+;;    (setq user-emacs-directory "/Users/PUJIE/Dropbox/Libraries/dotFiles/emacs.pujie.d/")
+;;  )
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-main)
 
